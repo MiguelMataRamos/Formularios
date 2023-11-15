43216bc3694126627fd38c11caff2ad2a8da14eb
EAG <mataramosmiguel@gmail.com>
2023-11-15 19:41:38 +0100

    viewBinding

--- app/src/main/java/com/example/formularios/Ejercicio2.kt
+++ app/src/main/java/com/example/formularios/Ejercicio2.kt
@@ -2,10 +2,21 @@ package com.example.formularios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.formularios.databinding.ActivityEjercicio2Binding
 
 class Ejercicio2 : AppCompatActivity() {
+    private lateinit var bind:ActivityEjercicio2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio2)
+        bind = ActivityEjercicio2Binding.inflate(layoutInflater)
+    }
+
+    fun registrar(view:View){
+        if (bind.nombre.text.isNullOrBlank()){
+
+        }
+
     }
 }
